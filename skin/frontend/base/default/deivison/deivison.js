a00e096313349ad29df6e49c4a9d1bb11c1f44da
Diego Souza <diego.souza@akatus.com>
2013-07-29 15:22:59 -0300

    ajax do cep funcionando tanto no cadastro como na compra

--- skin/frontend/base/default/deivison/deivison.js
+++ skin/frontend/base/default/deivison/deivison.js
@@ -122,21 +122,48 @@ jQuery(document).ready(function($j) {
         });
 
         function buscarEndereco(cep) {
-
-    			$j.get({
-    				url: 'http://cep.correiocontrol.com.br/' + cep + '.json', // trocar URL e fazer server-side
-    				type:'get',
-    				dataType: 'json',
-    				success:function(endereco){
+            if (/\d{8}/.test(cep)) {
+    			$j.getJSON('http://cep.correiocontrol.com.br/' + cep + '.json') // trocar URL e fazer server-side
+    				.done(function(endereco){
                         document.getElementById('street1').value = endereco.logradouro;
                         document.getElementById('street4').value = endereco.bairro;
-                        document.getElementById('city').value = endereco.cidade;
+                        document.getElementById('city').value = endereco.localidade;
 
                         // TODO: selecionar estado no select e definir region_id
 
+                        switch(endereco.uf) {
+                            case "AC": $j('#region_id').val(485); break;
+                            case "AL": $j('#region_id').val(486); break;
+                            case "AP": $j('#region_id').val(487); break;
+                            case "AM": $j('#region_id').val(488); break;
+                            case "BA": $j('#region_id').val(489); break;
+                            case "CE": $j('#region_id').val(490); break;
+                            case "ES": $j('#region_id').val(491); break;
+                            case "GO": $j('#region_id').val(492); break;
+                            case "MA": $j('#region_id').val(493); break;
+                            case "MT": $j('#region_id').val(494); break;
+                            case "MS": $j('#region_id').val(495); break;
+                            case "MG": $j('#region_id').val(496); break;
+                            case "PA": $j('#region_id').val(497); break;
+                            case "PB": $j('#region_id').val(498); break;
+                            case "PR": $j('#region_id').val(499); break;
+                            case "PE": $j('#region_id').val(500); break;
+                            case "PI": $j('#region_id').val(501); break;
+                            case "RJ": $j('#region_id').val(502); break;
+                            case "RN": $j('#region_id').val(503); break;
+                            case "RS": $j('#region_id').val(504); break;
+                            case "RO": $j('#region_id').val(505); break;
+                            case "RR": $j('#region_id').val(506); break;
+                            case "SC": $j('#region_id').val(507); break;
+                            case "SP": $j('#region_id').val(508); break;
+                            case "SE": $j('#region_id').val(509); break;
+                            case "TO": $j('#region_id').val(510); break;
+                            case "DF": $j('#region_id').val(511); break;
+                        }
+
                         document.getElementById('street2').focus();
-    				}
-    			});
+    			    });
+            }
 
         };
 
@@ -231,41 +258,3 @@ jQuery(document).ready(function($j) {
     'shipping-method': 1,   <- Atualiza os métodos de envio
     'review': 1             <- Atualiza o resumo da compra
 */
-
-//Para atualizar caso tenha desconto no boleto
-$j(function($) {
-      $j('input[name*="payment[method]"]').live('click', function() {
-              checkout.update({
-                    // 'review': 1
-                    //,'shipping-method': 1
-                    'payment-method': 1
-              });
-
-             setTimeout(function(){
-                        checkout.update({
-                            'review': 1
-                            //,'payment-method': 1
-                        });
-             }, 1000);
-
-              $j('html head').find('title').text(  "OSC: Finalizando compra no campo [ " + $j(this).attr('name') + " ]"  );
-              _gaq.push(['_trackPageview', '#' + $j(this).attr('name') + '']);
-              pageTracker._trackEvent("OSC no campo: ", "input_exit", $j(this).attr('name'));
-      });
-
-
-      $j('input[name*="shipping_method"]').live('click', function() {
-              checkout.update({
-                    'review': 1
-                    ,'payment-method': 1
-                    //'shipping-method': 1
-              });
-             setTimeout(function(){
-                        checkout.update({
-                            'review': 1,
-                            //'payment-method': 1
-                        });
-             }, 500);
-      });
-});
-
