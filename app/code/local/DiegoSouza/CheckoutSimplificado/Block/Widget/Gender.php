1e2482efb411faf1413a602346c56d6b65d449f9
Diego Souza <diego.souza@akatus.com>
2013-10-07 08:41:35 -0300

    Oops, esqueci de confirmar a remoção dos arquivos (Dob e Gender)

--- app/code/local/DiegoSouza/CheckoutSimplificado/Block/Widget/Gender.php
+++
@@ -1,9 +0,0 @@
-<?php
-class DiegoSouza_CheckoutSimplificado_Block_Widget_Gender extends Mage_Customer_Block_Widget_Gender
-{
-    public function _construct()
-    {
-        parent::_construct();
-        $this->setTemplate('checkoutsimplificado/widget/gender.phtml');
-    }
-}
